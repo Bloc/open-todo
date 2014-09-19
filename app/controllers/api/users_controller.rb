92ad8ece9783207e033d4a9d37386e87a317d3e0
LCmry <lcrees.2014@gmail.com>
2014-09-19 13:00:29 -0700

    Problems with user params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,9 +9,9 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to :index
+      respond_with @user
     else
-      redirect_to :index
+      render json: {error: 'There was a problem saving the user.'}
     end
   end
 
