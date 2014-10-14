772f1f88e95de9005aa7cc2ca2d1b4ecc861345f
Tien Yuan <tien.yuan@gmail.com>
2014-10-14 17:08:52 +0000

    added tests for update and destroy

--- spec/controllers/api/v1/lists_controller_spec.rb
+++ spec/controllers/api/v1/lists_controller_spec.rb
@@ -2,59 +2,7 @@ require 'spec_helper'
 
 describe Api::V1::ListsController do 
 
-  describe "#create" do
-
-    before do
-      user = create(:user, password: 'testpass')
-    end
-
-    context "with correct user's password" do
-      xit "takes a list name, creates it if it doesn't exist, and returns false if it does"
-
-      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open', 'password' => 'testpass' }}
-      post :create, params
-
-      expect(response).to be_success
-      expect(json).to eq(params['list'])
-      expect(List.last.name).to eq('testlist')
-    end
-
-    context "without correct user's password" do
-      xit "it errors"
-      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open', 'password' => 'wrongpass' }}
-      post :create, params
-
-      expect(response).to be_error
-    end
-
-    context "with blank password" do
-      xit "it errors"
-      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open' }}
-      post :create, params
-
-      expect(response).to be_error
-    end
-
-    context "without valid permission" do
-      xit "it errors"
-      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'wrongperm', 'password' => 'testpass' }}
-      post :create, params
-
-      expect(response).to be_error
-    end
-
-    context "with blank permission" do
-      xit "it errors"
-      params = { 'list' => { 'name' => 'testlist', 'password' => 'testpass' }}
-      post :create, params
-
-      expect(response).to be_error
-    end
-
-  end
-
   describe "#index" do
-
     before do
       user = create(:user, password: 'testpass')
       user2 = create(:user)
@@ -114,4 +62,110 @@ describe Api::V1::ListsController do
       )
     end
   end
+
+  describe "#create" do
+    before do
+      user = create(:user, password: 'testpass')
+    end
+
+    context "with correct user's password" do
+      xit "takes a list name, creates it if it doesn't exist, and returns false if it does"
+
+      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open', 'password' => 'testpass' }}
+      post :create, params
+
+      expect(response).to be_success
+      expect(json).to eq(params['list'])
+      expect(List.last.name).to eq('testlist')
+
+      post :create, params
+      expect(response).to be_error
+      expect(List.all.count).to eq 1      
+    end
+
+    context "without correct user's password" do
+      xit "it errors"
+      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open', 'password' => 'wrongpass' }}
+      post :create, params
+
+      expect(response).to be_error
+    end
+
+    context "with blank password" do
+      xit "it errors"
+      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open' }}
+      post :create, params
+
+      expect(response).to be_error
+    end
+
+    context "without valid permission" do
+      xit "it errors"
+      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'wrongperm', 'password' => 'testpass' }}
+      post :create, params
+
+      expect(response).to be_error
+    end
+
+    context "with blank permission" do
+      xit "it errors"
+      params = { 'list' => { 'name' => 'testlist', 'password' => 'testpass' }}
+      post :create, params
+
+      expect(response).to be_error
+    end
+  end
+
+  describe "#update" do
+    before do
+      @user = create(:user, password: 'testpass')
+      @list = create(:list, user: @user)
+    end
+
+    context "with correct user's password" do
+      xit "updates a list name"
+
+      params = { 'list' => { 'id' => '1', name => 'newlistname', 'password' => 'testpass' }}
+      put :update, params
+
+      expect(response).to be_success
+      expect(@list.name).to eq('newlistname')
+    end
+
+    context "without correct user's password" do
+      xit "it errors"
+      params = { 'list' => { 'name' => 'testlist', 'permissions' => 'open', 'password' => 'wrongpass' }}
+      post :create, params
+
+      expect(response).to be_error
+    end
+  end
+
+  describe '#destroy' do
+
+    before do
+      @user = create(:user, password: 'testpass')
+    end
+
+    xit "deletes a list" do
+      list = create(:list, user: @user)
+      delete :destroy, id: list.id
+
+      expect( response ).to be_success
+      expect( List.count ).to eq(0)
+    end
+
+    xit "fails to delete another user's list" do
+      other_user = create(:user)
+      list = create(:list, user: other_user)
+
+      assert_raises(ActiveRecord::RecordNotFound) do
+        delete :destroy, id: list.id
+      end
+
+      task.reload
+      expect(assigns(:list)).to be_nil
+    end
+  end
+
 end
