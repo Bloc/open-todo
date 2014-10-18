9d04eb1c62ac6a63f8bab85f43c6b65a88a152d5
Tien Yuan <tien.yuan@gmail.com>
2014-10-18 01:13:34 +0000

    fixed format for request spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
-describe "Users API" do
-  
-  it 'gets a list of users' do
-    @user = create(:user, username:"apitestuser123")
-    @api = create(:api_key)
-    get "/api/v1/users/", nil, {'X-ACCESS-TOKEN' => "#{@api.access_token}"}
-    expect(response).to eq(200)
-    json = JSON.parse(response.body)
-    expect(json).to include(@user.username)
-  end
+  describe "Users API" do
 
+    context "#index" do
+
+    before do
+      User.destroy_all
+      @api = create(:api_key)
+      30.times { create(:user) }
+      get "/api/v1/users/", nil, {'X-ACCESS-TOKEN' => "#{@api.access_token}"}
+    end
+
+    describe "should list all users" do
+      it { response.should be_ok }
+      it { JSON.parse(response.body)["users"].should be_a_kind_of(Array) }
+      it { JSON.parse(response.body)["users"].length.should eq 30 }
+    end
+  end
 end
