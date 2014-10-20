bd57272f321f17d7dac1883dbfb36a213504811e
Tien Yuan <tien.yuan@gmail.com>
2014-10-20 17:33:46 +0000

    switched to create_list

--- spec/requests/list_spec.rb
+++ spec/requests/list_spec.rb
@@ -5,16 +5,16 @@ describe "List API" do
     before do
       User.destroy_all
       @api = create(:api_key)
-      10.times { create(:list, permissions: "open") }
-      10.times { create(:list, permissions: "viewable") }
-      10.times { create(:list, permissions: "private") }
+      create_list(:list, 3, permissions: "open")
+      create_list(:list, 3, permissions: "viewable")
+      create_list(:list, 3, permissions: "private")
       get "/api/v1/lists/", nil, {'X-ACCESS-TOKEN' => "#{@api.access_token}"}
     end
 
     describe "should show all non-private lists" do
       it { expect(response.status).to eq(200) }
       it { json["lists"].should be_a_kind_of(Array) }
-      it { json["lists"].length.should eq 20 }
+      it { json["lists"].length.should eq 6 }
     end
   end
 
@@ -23,16 +23,16 @@ describe "List API" do
       User.destroy_all
       @user = create(:user)
       @api = create(:api_key, user: @user)
-      10.times { create(:list, permissions: "open", user: @user) }
-      10.times { create(:list, permissions: "viewable", user: @user) }
-      10.times { create(:list, permissions: "private", user: @user) }
+      create_list(:list, 3, permissions: "open", user: @user)
+      create_list(:list, 3, permissions: "viewable", user: @user)
+      create_list(:list, 3, permissions: "private", user: @user)
       get "/api/v1/users/#{@user.id}/lists", nil, {'X-ACCESS-TOKEN' => "#{@api.access_token}"}
     end
 
     describe "should show all lists" do
       it { expect(response.status).to eq(200) }
       it { json["lists"].should be_a_kind_of(Array) }
-      it { json["lists"].length.should eq 30 }
+      it { json["lists"].length.should eq 9 }
     end
   end
 
@@ -42,16 +42,16 @@ describe "List API" do
       @user1 = create(:user)
       @api = create(:api_key, user: @user1)
       @user2 = create(:user)
-      10.times { create(:list, permissions: "open", user: @user2) }
-      10.times { create(:list, permissions: "viewable", user: @user2) }
-      10.times { create(:list, permissions: "private", user: @user2) }
+      create_list(:list, 3, permissions: "open", user: @user2)
+      create_list(:list, 3, permissions: "viewable", user: @user2)
+      create_list(:list, 3, permissions: "private", user: @user2)
       get "/api/v1/users/#{@user2.id}/lists", nil, {'X-ACCESS-TOKEN' => "#{@api.access_token}"}
     end
 
     describe "should show non-private lists" do
       it { expect(response.status).to eq(200) }
       it { json["lists"].should be_a_kind_of(Array) }
-      it { json["lists"].length.should eq 20 }
+      it { json["lists"].length.should eq 6 }
     end
   end
 end
