d152463a25b0817c4acc2487fff0955e46cb6567
Tien Yuan <tien.yuan@gmail.com>
2014-10-20 17:27:45 +0000

    added spec for api

---
+++ spec/requests/authorization_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Authorization for API" do
+  context "connecting with an access_token" do
+    before do
+      @api = create(:api_key)
+      get "/api/v1/users/", nil, {'X-ACCESS-TOKEN' => "#{@api.access_token}"}
+    end
+
+    describe "should respond" do
+      it { expect(response.status).to eq(200) }
+    end
+  end
+
+  context "connecting without an access_token" do
+    before do
+      get "/api/v1/users/"
+    end
+
+    describe "should fail" do
+      it { expect(response.status).to eq(401) }
+    end
+  end  
+end
