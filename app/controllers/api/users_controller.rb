5fef6e1fcb1ac12b40c575401713d4e8a49060ac
josh-lang <joshuaclang1@gmail.com>
2015-03-14 21:50:40 -0400

    Play around with subdomain constraints and complete restful json api controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,30 +1,51 @@
 module Api
   class Api::UsersController < Api::ApiController
+    before_action :conditions_met?
+    before_action :set_user, only: [:show, :update, :destroy]
+
     def index
-      conditions_met?
-      users = User.all
-      render json: users, each_serializer: UserSerializer
+      @users = User.all
+      render json: @users, each_serializer: UserSerializer
     end
 
-    def create
-      conditions_met?
-      @user = User.new(user_params)
+    # def show
+    #   render json: @user
+    # end
 
+    def create
+      @user = User.new(user_params)
       if @user.save
-        render json: @user
+        render json: @user, status: :created, location: @user
       else
-        error(422, @user.errors.full_messages.join(', '))
+        render json: @user.errors, status: :unprocessable_entity
       end
     end
 
+    # def update
+    #   if @user.update(user_params)
+    #     head :no_content
+    #   else
+    #     render json: @user.errors, status: :unprocessable_entity
+    #   end
+    # end
+
+    # def destroy
+    #   @user.destroy
+    #   head :no_content
+    # end
+
     private
+
+    def conditions_met?
+      permission_denied_error unless true
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:username, :password)
-    end
-
-    def conditions_met?
-      return permission_denied_error unless true
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Todo::Application.routes.draw do
-  namespace :api do
-    resources :users, only: [:index, :create]
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    resources :users, only: [:index, :create] do
+      resources :lists, except: :index
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: :create
+    end
+
+    resources :items, only: :destroy
   end
 
   resources :users do
-    resources :lists, except: [:index]
+    resources :lists, except: :index
   end
 
   resources :lists, only: [] do
     resources :items, only: [:create, :new]
   end
 
-  resources :items, only: [:destroy]
-
-  root to: 'users#new'
+  resources :items, only: :destroy
 end
